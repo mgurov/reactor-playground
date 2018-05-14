2a8aa15e3054478819ef058078a08062d0d369a2
Mykola Gurov <ngurov@gmail.com>
2018-05-14 20:31:22 +0200

    interesting

--- src/test/java/com/github/mgurov/AppTest.java
+++ src/test/java/com/github/mgurov/AppTest.java
@@ -1,11 +1,13 @@
 package com.github.mgurov;
 
 import org.junit.Test;
+import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 import reactor.util.function.Tuple2;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 public class AppTest {
 
@@ -18,8 +20,26 @@ public class AppTest {
 
         Flux<Tuple2<String, String>> zipped = seq1.zipWith(seq2);
 
+
+        Flux<String> composed = zipped.compose(new Function<Flux<Tuple2<String, String>>, Publisher<String>>() {
+            @Override
+            public Publisher<String> apply(Flux<Tuple2<String, String>> tuple2Flux) {
+                return tuple2Flux.map(new Function<Tuple2<String, String>, String>() {
+                    @Override
+                    public String apply(Tuple2<String, String> objects) {
+                        return objects.getT1() + objects.getT2();
+                    }
+                });
+            }
+        });
+
+        composed.subscribe((t) -> {
+            System.out.println("composed: " + t);
+        });
+
         zipped.subscribe((t) -> {
             System.out.println("zipped: " + t);}
-            );
+        );
+
     }
 }
